2bc13ba670badffe4287a6483edfc38cb69fe4b7
aenyeart <reallyhardtoforget@gmail.com>
2021-01-22 18:18:51 -0800

    Changes newPolitician() to generate full name and last name

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let newPolitician = function(newName, partyColor) {
+let newPolitician = function(firstName, lastName, partyColor) {
   let politician = {
-    name: newName,
+    name: firstName + " " + lastName,
+    lastName: lastName,
     color: partyColor,
     electionResults: null,
     totalVotes: 0,
@@ -15,8 +16,8 @@ let newPolitician = function(newName, partyColor) {
   return politician;
 };
 
-let booger = newPolitician("Boris Boogerbottom", [132, 17, 11]);
-let pickle = newPolitician("Penelope Picklenose", [245, 141, 136]);
+let booger = newPolitician("Boris", "Boogerbottom", [132, 17, 11]);
+let pickle = newPolitician("Penelope", "Picklenose", [245, 141, 136]);
 
 booger.electionResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
 pickle.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
@@ -36,7 +37,7 @@ amendResults(43, 11, 27); // TX (state 43), booger: 11, pickle: 27
 
 // Determines individual state winner and changes color of state on map to match the winner. Gets called by State.prototype.mouseIn() /found in map.js/ when a state is moused over.
 let setStateResults = function(state) {
-  theStates[state].winner = null; // TODO: seems redundant of map.js line224
+  // theStates[state].winner = null; // TODO: seems redundant of map.js line224
   if (booger.electionResults[state] > pickle.electionResults[state]) {
     theStates[state].winner = booger;
   } else if (pickle.electionResults[state] > booger.electionResults[state]) {
@@ -49,7 +50,8 @@ let setStateResults = function(state) {
     theStates[state].rgbColor = [11, 32, 57];
   }
 
-  // Gets at the table displaying individual states' results when states are moused over.
+  // Gets at the state results table displaying individual states' results when states are moused over.
+  // TODO: This section could be integrated better with the function above, and some elements could be defined on page load/outside this function so that the candidates names populate their respective fields *before* mouseIn events.
   let stateTable = document.getElementById("stateResults");
   let stateTableHeader = stateTable.children[0];
   let stateTableBody = stateTable.children[1];
@@ -65,15 +67,18 @@ let setStateResults = function(state) {
   // Fills those fields in the state table with data
   stateName.innerText = theStates[state].nameFull;
   stateAbbrev.innerText = theStates[state].nameAbbrev;
-  candidate1Name.innerText = ;
-  candidate1Results.innerText = "infinity";
-  candidate2Name.innerText = "me";
-  candidate2Results.innerText = "0";
-  stateWinnerName.innerText = "you";
+  candidate1Name.innerText = booger.lastName;
+  candidate1Results.innerText = booger.electionResults[state];
+  candidate2Name.innerText = pickle.lastName;
+  candidate2Results.innerText = pickle.electionResults[state];
+
+  if (booger.electionResults[state] == pickle.electionResults[state]) {
+    stateWinnerName.innerText = "DRAW";
+  } else {
+    stateWinnerName.innerText = theStates[state].winner.lastName;
+  }
+  // END state results table
 };
-
-
-
 
 
 // vvv This is a method of the newPolitician function, so it is declared there.
